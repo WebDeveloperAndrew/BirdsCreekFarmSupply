24d48888f78292bd886aedffeb415cb41cdfee59
unknown <someonefromcanada38@gmail.com>
2018-10-18 12:34:31 -0400

    Updated Mongo Database

--- BirdsCreekNodeBackend/Node/app/databaseApi.js
+++ BirdsCreekNodeBackend/Node/app/databaseApi.js
@@ -1,28 +1,29 @@
+module.exports = function(app) {
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var dburl = process.env.MONGO_URI;
+mongoose.connect(dburl);
+
 var productSchema = new Schema({
-    id: {type:Integer , unique : true},
     name: {type:String , required : true},
     image: {type:String , required : true},
     description: {type:String, required : true},
     info: { type:String, required: false },
-    price: { type: Double, required: false },
+    price: { type:Number, required: false },
     brand: {type:String, default: 'none'}
   }, { collection: 'products' });
   
 var product = mongoose.model('product', productSchema);
 
-app.post('/api/createproduct', newProduct);
+app.post('/api/createproduct', createProduct);
 app.post('/api/getproduct', getProduct);
+app.post('/api/updateproduct', updateProduct);
+app.post('/api/searchproduct', searchProduct);
+app.post('/api/deletedatabase', deleteDatabase);
+app.post('/api/deleteproduct', deleteProduct);
 
-function validateProduct(store)
-{
-  if(store.Name && store.Image && store.Description)
-  {
-    return true;
-  }
-  return false;
-}
-
-function newProduct(req, res)
+function createProduct(req, res)
 {
   var store = '';
   req.on('data', function(data) 
@@ -36,10 +37,9 @@ function newProduct(req, res)
     catch(e)
     {
       console.log(e);
-      console.log("Could Not Parse JSON data");
+      console.error("Could Not Parse JSON data");
       res.send("Could Not Parse JSON data")
     }
-   // if( validateProduct(store))
     var prod = new product();
     prod.name = store.name;
     prod.image = store.image;
@@ -51,14 +51,75 @@ function newProduct(req, res)
     prod.save(function(error) {
       if(error)
       {
-        console.log(parseError(error));
-        res.send(parseError(error));
+        console.error('Error saving data');
+        res.send(error);
       }
       else
       {
         console.log("Success: Product data created");
         res.send({"Success":"Product data created"});
       }
+    });
+  });
+}
+
+function updateProduct(req, res)
+{
+  var store = '';
+  req.on('data', function(data) 
+  {
+      store += data;
+  });
+  req.on('end', function(){
+    try {
+     store = JSON.parse(store);
+    }
+    catch(e)
+    {
+      console.log(e);
+      console.error("Could Not Parse JSON data");
+      res.send("Could Not Parse JSON data")
+    }
+    var updates;
+
+    if(store.name)
+    {
+      updates.push("name", store.name);
+    }
+    if(store.image)
+    {
+      updates.push("image", store.image);
+    }
+    if(store.description)
+    {
+      updates.push("description", store.description);
+    }
+    if(store.info)
+    {
+      updates.push("info", store.info);
+    }
+    if(store.price)
+    {
+      updates.push("price", store.price);
+    }
+    if(store.brand)
+    {
+      updates.push("brand", store.brand);
+    }
+
+
+    product.findByIdAndUpdate(store.id, {$set: updates}, function (error, prod) {
+      if(error)
+      {
+        console.error('Error updating data');
+        res.send(error);
+      }
+      else
+      {
+        console.log("Success: Product data updated");
+        res.send({"Success":"Product data updated", "Data": prod });
+      }
+
     });
   });
 }
@@ -74,7 +135,7 @@ function getProduct(req, res)
     {
       store = JSON.parse(store);
       console.log(store);
-      job.findById(store.id, function(err, result){
+      product.findById(store.id, function(err, result){
         if(err) throw err;
         console.log(result);
         if(result){
@@ -89,9 +150,81 @@ function getProduct(req, res)
         }
         else
         {
-          console.log("Error: The id was not found");
+          console.error("Error: The id was not found");
           res.send({"Error": "The id was not found"});
         }
       });
     });
   }
+
+  function searchProduct(req,res)
+  {
+    var store = '';
+    req.on('data', function(data) 
+    {
+        store += data;
+    });
+    req.on('end', function() 
+    {
+      store = JSON.parse(store);
+      query = new RegExp(store.query, "i");
+      console.log(store);
+      product.find({$or:[{'name':query},{'image':query},{'description':query},{'info':query},{'brand':query}]}, function(err, result){
+        if(err){ 
+          console.error('search error')
+          console.error(err);
+          throw err;
+        }
+        if(result){
+          console.log(result);
+          res.send(result);
+        }
+        else
+        {
+          console.error("Error: The id was not found");
+          res.send({"Error": "The id was not found"});
+        }
+      });
+    });
+  }
+  function deleteProduct(req,res)
+  {
+    var store = '';
+    req.on('data', function(data) 
+    {
+        store += data;
+    });
+    req.on('end', function() 
+    {
+      store = JSON.parse(store);
+      console.log(store);
+      product.findByIdAndRemove(store.id, function(err){
+        if(err){ 
+          console.error('search error')
+          console.error(err);
+          throw err;
+        }
+        else
+        {
+          console.log("Removed: "+store.id);
+          res.send({"Removed": store.id});
+        }
+      });
+    });
+  }
+  function deleteDatabase(req,res)
+  {
+    product.deleteMany({}, function(err){
+      if(err){ 
+        console.error('Delete error')
+        console.error(err);
+        throw err;
+      }
+      else
+      {
+        console.log("Database Deleted");
+        res.send({"Removed": "Database"});
+      }
+    });
+  }
+}
--- BirdsCreekNodeBackend/Node/server.js
+++ BirdsCreekNodeBackend/Node/server.js
@@ -1,28 +1,27 @@
 require('dotenv').config();
 var express = require('express');
+var app = express();
 var mongo = require('mongodb');
-var rmapi = require('./app/databaseApi.js');
+var databaseApi = require('./app/databaseApi.js');
 var dburl = process.env.MONGO_URI;
-
-app.use("/public", express.static(path.join(__dirname, 'public')));
 
 mongo.MongoClient.connect(dburl, function(err, db) {
 
     if (err) {
+      console.log(err);
       throw new Error('Error: Database not connected');
     } else {
       console.log('Connected to MongoDB');
-    }
-    
-    db.createCollection("products", {
-      capped: false
-    });
+      var dbase = db.db("BCData")
+      dbase.createCollection("products", {
+        capped: false
+      });
 
-    databaseApi(app,db);
-    
-    var port = process.env.PORT || 8080;
-    app.listen(port, function() {
-      console.log('Node.js listening on port ' + port);
-    });
-    
+      databaseApi(app);
+      
+      var port = process.env.PORT || 8080;
+      app.listen(port, function() {
+        console.log('Node.js listening on port ' + port);
+      });
+    }
   });
